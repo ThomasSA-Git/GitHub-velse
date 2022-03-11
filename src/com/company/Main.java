1e7371c675a39a5082623f7aec97104610dfd6ca
RodrigoPerea <chri65mb@stud.kea.dk>
2022-03-11 14:03:41 +0100

    Ny test Rodrigo slettet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Fejltest");
         System.out.println("Check");
-        System.out.println("fejl");
-        System.out.println("hola");
+
         System.out.println("hola 2");
         System.out.println("hej");
     }
