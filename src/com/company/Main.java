593c81ba0f41bbaffed8985fbdbfd6852ca05bed
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-11 14:04:08 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
         System.out.println("Fejltest");
         System.out.println("Check");
         System.out.println("fejl");
-        System.out.println("hola");
-        System.out.println("hola 2");
-        System.out.println("hej");
     }
 }
