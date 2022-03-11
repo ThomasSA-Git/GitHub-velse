d36f07ed028d2d9cd0fa183a02a8eeacdcdf2009
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-11 14:09:04 +0100

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
 
+    public void testGit(){
+        System.out.println("Metode");
+    }
+
     public static void main(String[] args) {
+        Main test = new Main();
+        test.testGit();
+
         System.out.println("Hello world");
         System.out.println("Fejltest");
         System.out.println("Check");
